217a8321abd2b7015fad35e541c7bcba5cc63d41
tduringer <trintduringer@gmail.com>
2025-01-20 11:12:52 -0800

    Add core package file structure and files.

---
+++ composeApp/src/commonMain/kotlin/com/trintduringer/book_search_cmp/core/domain/DataError.kt
@@ -0,0 +1,17 @@
+package com.trintduringer.book_search_cmp.core.domain
+
+sealed interface DataError: Error {
+    enum class Remote: DataError {
+        REQUEST_TIMEOUT,
+        TOO_MANY_REQUESTS,
+        NO_INTERNET,
+        SERVER,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local: DataError {
+        DISK_FULL,
+        UNKNOWN
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/trintduringer/book_search_cmp/core/domain/Error.kt
@@ -0,0 +1,4 @@
+package com.trintduringer.book_search_cmp.core.domain
+
+interface Error {
+}
